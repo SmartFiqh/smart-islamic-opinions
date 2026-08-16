1a46a04f1ef024ff1bb606c7602877c874a4a4d9
smartapps <babuker.apps@gmail.com>
2026-08-16 23:17:01 +0200

    Update language_provider.dart

--- lib/providers/language_provider.dart
+++ lib/providers/language_provider.dart
@@ -9,19 +9,4 @@ class LanguageProvider extends ChangeNotifier {
     _currentLocale = Locale(languageCode);
     notifyListeners();
   }
-
-  String getCurrentLanguageName() {
-    switch (_currentLocale.languageCode) {
-      case 'en':
-        return 'English';
-      case 'fa':
-        return 'فارسی';
-      case 'ur':
-        return 'اُردُو';
-      case 'id':
-        return 'Bahasa Indonesia';
-      default:
-        return 'العربية';
-    }
-  }
 }
