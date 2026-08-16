36d75e04dc6358097308a69291990e9485e104f8
smartapps <babuker.apps@gmail.com>
2026-08-16 15:57:59 +0200

    Add CommentModel class for comment handling

---
+++ lib/models/comment_model.dart
@@ -0,0 +1,21 @@
+class CommentModel {
+  final String id;
+  final String issueId;
+  final String userName;
+  final String commentText;
+  final double rating;
+  final String type; // 'suggestion', 'correction', 'appreciation', 'other'
+  final DateTime timestamp;
+  final bool isApproved;
+
+  CommentModel({
+    required this.id,
+    required this.issueId,
+    required this.userName,
+    required this.commentText,
+    required this.rating,
+    required this.type,
+    required this.timestamp,
+    this.isApproved = false,
+  });
+}
