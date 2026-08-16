090a6d13deb43931e5d4bce67fbb9520101cf1e1
smartapps <babuker.apps@gmail.com>
2026-08-16 23:16:29 +0200

    Update filter_provider.dart

--- lib/providers/filter_provider.dart
+++ lib/providers/filter_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../models/madhhab_model.dart';
 
 class FilterProvider extends ChangeNotifier {
-  String _selectedGroup = 'all'; // all, Sunni, Shia, Ibadi, Other
+  String _selectedGroup = 'all';
   String? _selectedMadhabId;
 
   String get selectedGroup => _selectedGroup;
@@ -25,35 +25,23 @@ class FilterProvider extends ChangeNotifier {
       return [Madhhab.getById(_selectedMadhabId!)];
     }
     switch (_selectedGroup) {
-      case 'all':
-        return Madhhab.all;
-      case 'Sunni':
-        return Madhhab.all.where((m) => m.group == 'Sunni').toList();
-      case 'Shia':
-        return Madhhab.all.where((m) => m.group == 'Shia').toList();
-      case 'Ibadi':
-        return Madhhab.all.where((m) => m.group == 'Ibadi').toList();
-      case 'Other':
-        return Madhhab.all.where((m) => m.group == 'Other').toList();
-      default:
-        return Madhhab.all;
+      case 'all': return Madhhab.all;
+      case 'Sunni': return Madhhab.all.where((m) => m.group == 'Sunni').toList();
+      case 'Shia': return Madhhab.all.where((m) => m.group == 'Shia').toList();
+      case 'Ibadi': return Madhhab.all.where((m) => m.group == 'Ibadi').toList();
+      case 'Other': return Madhhab.all.where((m) => m.group == 'Other').toList();
+      default: return Madhhab.all;
     }
   }
 
   String getCurrentGroupName(String lang) {
     switch (_selectedGroup) {
-      case 'all':
-        return lang == 'ar' ? 'جميع المذاهب' : 'All Schools';
-      case 'Sunni':
-        return lang == 'ar' ? 'مذاهب السنة' : 'Sunni Schools';
-      case 'Shia':
-        return lang == 'ar' ? 'مذاهب الشيعة' : 'Shia Schools';
-      case 'Ibadi':
-        return lang == 'ar' ? 'المذهب الإباضي' : 'Ibadi School';
-      case 'Other':
-        return lang == 'ar' ? 'آراء أخرى' : 'Other Views';
-      default:
-        return lang == 'ar' ? 'مخصص' : 'Custom';
+      case 'all': return lang == 'ar' ? 'جميع المذاهب' : 'All Schools';
+      case 'Sunni': return lang == 'ar' ? 'مذاهب السنة' : 'Sunni Schools';
+      case 'Shia': return lang == 'ar' ? 'مذاهب الشيعة' : 'Shia Schools';
+      case 'Ibadi': return lang == 'ar' ? 'المذهب الإباضي' : 'Ibadi School';
+      case 'Other': return lang == 'ar' ? 'آراء أخرى' : 'Other Views';
+      default: return lang == 'ar' ? 'مخصص' : 'Custom';
     }
   }
 }
